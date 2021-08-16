6b1052a49ce1d072653de90c96d5aee870d85967
BohdanOmelianec <omelianecb@gmail.com>
2021-08-16 16:41:01 +0300

    first step

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import './App.css';
 import React from 'react';
-import {useSelector, useDispatch} from 'react-redux';
+import {Link, Route} from 'react-router-dom';
+import Home from './components/Home';
+import styled from 'styled-components';
 
+
+const Wrapper = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+
+`;
+
+const Navigator = styled.div`
+  padding: 50px;
+  height: 100vh;
+  background: #efefef;
+`;
+
+const Content = styled.div`
+  width: 100%;
+`;
 
 export default function App() {
-  const storage = useSelector(state => state.data);
-  const dispatch = useDispatch();
-
-  const incrementData = () => ({
-    type: 'increment'
-  });
-  const decrement = () => ({
-    type: 'decrement'
-  });
-
   return (
-    <>
-      <p>state: {storage}</p>
-      <button onClick={() => dispatch(incrementData())}>Incr</button>
-      <button onClick={() => dispatch(decrement())}>Decr</button>
-    </>
+    <Wrapper>
+      <Navigator>
+        <Link exact to='/cursor-project'>Home</Link>
+      </Navigator>
+      <Content>
+        <div style={{height: '100px', textAlign: 'right'}}>Balance: $2500</div>
+        <Route path='/cursor-project' component={Home}/>
+      </Content>
+    </Wrapper>
   );
 }
