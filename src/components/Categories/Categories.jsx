86b50035238a7fb974d212be388ef0558dce24e9
Arsenii Netrebenko <boecrejha@gmail.com>
2021-08-16 18:51:20 +0300

    📦 NEW: Elements

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -2,11 +2,14 @@
 import React from 'react'
 import Flex from '../StyledComponents/Flex'
 import Title from '../StyledComponents/Title'
+import Button from '../StyledComponents/Button'
+
 
 export default function Categories() {
   return (
     <Flex>
-      <Title>Categoties</Title>
+      <Title>Categories</Title>
+      <Button>Add more</Button>
     </Flex>
   )
 }
