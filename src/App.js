008a67d8469ee275692f24fcac8546477c2e0ceb
BohdanOmelianec <omelianecb@gmail.com>
2021-08-19 21:32:31 +0300

    add default page

--- src/App.js
+++ src/App.js
@@ -95,7 +95,6 @@ export default function App() {
         <Switch>
           <Route path='/cursor-project'>
             <Home removeItem={removeItem} dataArr={dataArr} setData={setData}/>
-            <Redirect to='/charges'/>
           </Route>
           <Route exact path='/categories' component={Categories}/>
           <Route path='/charts' component={Chart}/>
