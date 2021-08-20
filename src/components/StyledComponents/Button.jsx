e897242d19dd6b643070310147ed2eede5493672
Arsenii Netrebenko <boecrejha@gmail.com>
2021-08-20 23:50:31 +0300

    📦 NEW: Init style

--- src/components/StyledComponents/Button.jsx
+++ src/components/StyledComponents/Button.jsx
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 const StyledButton = styled.button`
 	width: ${({ width }) => width || '100px'};
 	height: ${({ height }) => height || '40px'};
-	background-color: ${({bColor}) => bColor || 'blue'};
+	background-color: ${({ bColor }) => bColor || 'rgb(90,	141,	238	)'};
 	color: ${({ color }) => color || 'white'};
 	font-weight: 500;
 	padding: ${({ padding }) => padding || '2px'};
