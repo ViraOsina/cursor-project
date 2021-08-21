790c897674dab521510a3742730a371fbbb055d8
Arsenii Netrebenko <boecrejha@gmail.com>
2021-08-22 01:57:36 +0300

    📦 NEW: Global style instead css

--- src/index.js
+++ src/index.js
@@ -4,9 +4,27 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import App from './App'
 import { store } from './redux/store'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
+`
 
 ReactDOM.render(
 	<Provider store={store}>
+		<GlobalStyle />
 		<Router>
 			<App />
 		</Router>
