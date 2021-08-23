e2b4f7ad90d2cda99a279d8fc3d6f51c2e7801c8
Arsenii Netrebenko <boecrejha@gmail.com>
2021-08-23 18:27:17 +0300

    📦 NEW: Saving redux state to localStorage and using it for saving custom categories

---
+++ src/redux/state.js
@@ -0,0 +1,48 @@
+import Icons from '../components/Icons'
+
+export const initialState = {
+	categories: [
+		{
+			id: Math.trunc(Math.random() * Date.now()),
+			name: 'Food',
+			icon: Icons.Food,
+			description: 'For all my food',
+			date: '21 Jul',
+			standard: true,
+		},
+		{
+			id: Math.trunc(Math.random() * Date.now()),
+			name: 'Clothes',
+			icon: Icons.Clothes,
+			description: 'Clothing costs',
+			date: '9 Jun',
+			standard: true,
+		},
+		{
+			id: Math.trunc(Math.random() * Date.now()),
+			name: 'Restaurants',
+			icon: Icons.Restaurants,
+			description: 'The cost of going to the restaurant',
+			date: '17 Dec',
+			standard: true,
+		},
+		{
+			id: Math.trunc(Math.random() * Date.now()),
+			name: 'Utility bills',
+			icon: Icons.UtilityBills,
+			description: 'Who invented the water bills?',
+			date: '14 Nov',
+			standard: true,
+		},
+		{
+			id: Math.trunc(Math.random() * Date.now()),
+			name: 'Pets',
+			icon: Icons.Pets,
+			description: 'Shopping for a tailed friend',
+			date: '21 Oct',
+			standard: true,
+		},
+	],
+}
+
+
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,3 +3,8 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import { rootReducer } from './rootReducer'
 
 export const store = createStore(rootReducer, composeWithDevTools())
+
+store.subscribe(() => {
+	localStorage['reduxState'] = JSON.stringify(store.getState())
+})
+
