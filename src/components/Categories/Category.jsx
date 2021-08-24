114d9be9c786c1d12f5654bda0117ea6748e826d
Arsenii Netrebenko <boecrejha@gmail.com>
2021-08-24 18:04:38 +0300

    📦 NEW: Editing option for custom categories

--- src/components/Categories/Category.jsx
+++ src/components/Categories/Category.jsx
@@ -1,30 +1,45 @@
-import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import { faPen, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { removeCategoryAction } from '../../redux/categoryReducer'
+import Icons from '../Icons'
 import TableRow from '../StyledComponents/TableRow'
 import TableRowItem from '../StyledComponents/TableRowItem'
 import TextContent from '../StyledComponents/TextContent'
+import ModalEditCategory from './ModalEditCategory'
 
 export default function Category(props) {
 	const dispatch = useDispatch()
+	const [editCategoryModalActive, setEditCategoryModalActive] = useState(false)
 
-	function removeActionHandler(id) {
+	function removeCategoryActionHandler(id) {
 		dispatch(removeCategoryAction(id))
 	}
 
-	let removeIcon = null
+	let removeButton = null
+	let editButton = null
 
 	if (!props.standard) {
-		removeIcon = (
+		removeButton = (
 			<FontAwesomeIcon
-				onClick={() => removeActionHandler(props.id)}
+				onClick={() => removeCategoryActionHandler(props.id)}
 				icon={faTrashAlt}
 				style={{ cursor: 'pointer' }}
 			/>
 		)
+		editButton = (
+			<FontAwesomeIcon
+				onClick={() => setEditCategoryModalActive(true)}
+				icon={faPen}
+				style={{ cursor: 'pointer' }}
+			/>
+		)
 	}
+
+
+
+	const category = {...props}
 
 	return (
 		<TableRow>
@@ -34,7 +49,16 @@ export default function Category(props) {
 			</TableRowItem>
 			<TableRowItem>{props.description}</TableRowItem>
 			<TableRowItem>{props.date}</TableRowItem>
-			<TableRowItem>{removeIcon}</TableRowItem>
+			<TableRowItem>
+				<TableRowItem>{editButton}</TableRowItem>
+				<TableRowItem margin="10px">{removeButton}</TableRowItem>
+			</TableRowItem>
+			<ModalEditCategory
+				active={editCategoryModalActive}
+				setActive={setEditCategoryModalActive}
+				icons={Icons}
+				category={category}
+			/>
 		</TableRow>
 	)
 }
--- src/redux/categoryReducer.js
+++ src/redux/categoryReducer.js
@@ -1,5 +1,5 @@
 import Icons from '../components/Icons'
-import { ADD_CATEGORY, REMOVE_CATEGORY } from './actionTypes'
+import { ADD_CATEGORY, EDIT_CATEGORY, REMOVE_CATEGORY } from './actionTypes'
 import moment from 'moment'
 import { initialState } from './state'
 
@@ -17,6 +17,18 @@ export const categoryReducer = (state = initialState, action) => {
 					...state.categories.filter(
 						category => category.id !== action.payload
 					),
+				],
+			}
+		case EDIT_CATEGORY:
+			return {
+				...state,
+				categories: [
+					...state.categories.map(category => {
+						if (category.id === action.payload.id) {
+							return action.payload
+						}
+						return category
+					}),
 				],
 			}
 		default:
@@ -39,4 +51,16 @@ export const addCategoryAction = payload => ({
 export const removeCategoryAction = payload => ({
 	type: REMOVE_CATEGORY,
 	payload,
+})
+
+export const editCategoryAction = payload => ({
+	type: EDIT_CATEGORY,
+	payload: {
+		id: payload.id,
+		name: payload.name,
+		icon: Icons[payload.icon],
+		description: payload.description,
+		date: moment().format('DD MMM'),
+		standard: false,
+	},
 })
