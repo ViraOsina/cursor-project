86949d2af5ede8f4fa05d9f5f30ed8ef26ec8540
BohdanOmelianec <omelianecb@gmail.com>
2021-08-29 12:12:48 +0300

    created sort inputs

--- src/components/Home/Charges.jsx
+++ src/components/Home/Charges.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import TableRow from './TableRow';
 import ModalForm from './modalComponent_addMore/Modal';
 import { useDispatch, useSelector } from 'react-redux';
-import {openModalAction} from '../../redux/homeReducer';
+import { openModalAction } from '../../redux/homeReducer';
 import EditModal from './modalComponent_addMore/EditModal';
 
 
@@ -51,6 +51,7 @@ const Span = styled.span`
 const Select = styled.select`
     font-size: 18px;
     padding: 5px;
+    margin: 0 10px;
     border: none;
     outline: none;
     color: #5f5f5f;
@@ -69,55 +70,96 @@ const AddMore = styled.button`
 
 export default function Charges() {
     const dispach = useDispatch();
-    const chargesDB =  useSelector(state => state.homeReducer.chargesDB);
+    const chargesDB = useSelector(state => state.homeReducer.chargesDB);
     const targetItem = useSelector(state => state.homeReducer.targetItem);
     const categories = useSelector(state => state.category.categories);
-
+    const [alphabetSort, setAlphabetSort] = useState('');
+    const [moneySort, setMoneySort] = useState('');
     const [selectCategory, setSelectCategory] = useState('');
     const [inputMoney, setInputMoney] = useState('');
-    const filteredChargesArr = selectCategory && inputMoney ? chargesDB.filter(item => item.category === selectCategory && item.money <= inputMoney)
-     : selectCategory ? chargesDB.filter(item => item.category === selectCategory) 
-     : inputMoney ? chargesDB.filter(item => item.money <= inputMoney) : chargesDB;
+    let filteredChargesArr = [...chargesDB];
 
-    return(
-    <>
-        <Filter>
-            <InputBlock>
-                <Span>My Charges</Span>
-                <Select value={selectCategory} onChange={(e) => {
-                    setSelectCategory(e.target.value);
-                }}> <option value={''}>Select category</option>
-                    {categories.map(category => {
-                        return(
-                            <option key={category.id}>{category.name}</option>
-                        )
-					})}
-                </Select>
-                <Input
-                    onChange={e => {
-                        setInputMoney(e.target.value)
-                    }}
-                    value={inputMoney}
-                    required
-                    placeholder="Money"
-                    name="money"
-                    type="number"
-                />
-            </InputBlock>
-            <AddMore onClick={() => {dispach(openModalAction())}}>Add more</AddMore>
-        </Filter>
-        <Table>
-            <span>Category</span>
-            <span>Description</span>
-            <span>Date</span>
-            <span>Money</span>
-            <span>Action</span>
-        </Table>
-        {filteredChargesArr.map((item, index, arr) => {
-            return <TableRow key={index} removeId={index} data={item} arr={arr} target={'charges'}/>
-        })}
-        <ModalForm target='charges'/>
-        {targetItem ? <EditModal target={'charges'}/> : null}
-    </>
+
+// Sort
+    if (alphabetSort) {
+        filteredChargesArr=filteredChargesArr.sort((a, b) => {
+            return alphabetSort === 'true' ? (a.category > b.category ? 1 : -1) : (a.category < b.category ? 1 : -1)
+        });
+    } else if (moneySort) {
+        filteredChargesArr=filteredChargesArr.sort((a, b) => {
+            return moneySort === 'true' ? a.money - b.money : b.money - a.money;
+        })
+    } else {
+        filteredChargesArr = [...chargesDB];
+    }
+
+// Filter
+    filteredChargesArr = selectCategory && inputMoney 
+        ? chargesDB.filter(item => item.category === selectCategory && item.money <= inputMoney)
+        : selectCategory ? chargesDB.filter(item => item.category === selectCategory)
+        : inputMoney ? chargesDB.filter(item => item.money <= inputMoney) : filteredChargesArr;
+
+
+
+    return (
+        <>
+            <Filter>
+                <InputBlock>
+                    <Span>Filter: </Span>
+                    <Select value={selectCategory} onChange={(e) => {
+                        setSelectCategory(e.target.value);
+                    }}> <option value={''}>Select category</option>
+                        {categories.map(category => {
+                            return (
+                                <option key={category.id}>{category.name}</option>
+                            )
+                        })}
+                    </Select>
+                    <Input
+                        onChange={e => {
+                            setInputMoney(e.target.value)
+
+                        }}
+                        value={inputMoney}
+                        required
+                        placeholder="Money"
+                        name="money"
+                        type="number"
+                    />
+                </InputBlock>
+                <AddMore onClick={() => { dispach(openModalAction()) }}>Add more</AddMore>
+            </Filter>
+            <Filter>
+                <InputBlock>
+                    <Span>Sort: </Span>
+                    <Select onChange={e => {
+                        setAlphabetSort(e.target.value)
+                    }}>
+                        <option value={''}>---</option>
+                        <option value={true}>A - Z</option>
+                        <option value={false}>Z - A</option>
+                    </Select>
+                    <Select onChange={e => {
+                        setMoneySort(e.target.value)
+                    }}>
+                        <option value={''}>---</option>
+                        <option value={true}>From lower</option>
+                        <option value={false}>From higher</option>
+                    </Select>
+                </InputBlock>
+            </Filter>
+            <Table>
+                <span>Category</span>
+                <span>Description</span>
+                <span>Date</span>
+                <span>Money</span>
+                <span>Action</span>
+            </Table>
+            {filteredChargesArr.map((item, index, arr) => {
+                return <TableRow key={index} removeId={index} data={item} arr={arr} target={'charges'} />
+            })}
+            <ModalForm target='charges' />
+            {targetItem ? <EditModal target={'charges'} /> : null}
+        </>
     )
 }
